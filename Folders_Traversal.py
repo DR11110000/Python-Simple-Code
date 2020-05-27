9f24f4123c327029fff8af4b90c5dc0f7f534e1b
unknown <dubeyravivinod@gmail.com>
2020-05-27 21:50:01 +0530

    Python simple code for get the file list from the subfolders

---
+++ Folders_Traversal.py
@@ -0,0 +1,26 @@
+import os, sys
+import argparse
+
+args = sys.argv
+args.pop(0)
+arg_parser = argparse.ArgumentParser("Help")
+arg_parser.add_argument("file_type", type =str , help = "folder", default = 1)
+file_type = sys.argv[0]
+
+def process(file_type):
+	filelist = []
+	for root, dirs, files in os.walk(file_type):
+		for file in files:
+			filelist.append(os.path.join(root, file))
+
+
+	for name in filelist:
+		with open('F:\\file_name.txt', 'a') as f:
+			f.write(name)
+			f.write('\n')
+			f.close()
+
+	return 
+
+process(file_type)
+
